6337e93b1fe84b7968e93c27cd3acb06c8495877
Miguel Hernández Romero <miguel2hermanas@gmail.com>
2014-04-12 13:57:32 +0200

    Update userdelStack.py

--- userdelStack.py
+++ userdelStack.py
@@ -1,27 +1,32 @@
 import novaclient.v1_1.client as nvclient
 from keystoneclient.v2_0 import client
 
-
+import sys 
 import os
 
-def get_keystone_creds():
-    d = {}
-    d['username'] = os.environ['OS_USERNAME']
-    d['password'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['tenant_name'] = os.environ['OS_TENANT_NAME']
-    return d
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print "Por favor, si quiere eliminar un usuario, la sitaxis es"
+    print "userdelStack.py <usuario>"
 
-def get_nova_creds():
-    d = {}
-    d['username'] = os.environ['OS_USERNAME']
-    d['api_key'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['project_id'] = os.environ['OS_TENANT_NAME']
-    return d
+else:
+    def get_keystone_creds():len(sys.argv
+        d = {}
+        d['username'] = os.environ['OS_USERNAME']
+        d['password'] = os.environ['OS_PASSWORD']
+        d['auth_url'] = os.environ['OS_AUTH_URL']
+        d['tenant_name'] = os.environ['OS_TENANT_NAME']
+        return d
 
-creds = get_nova_creds()
-creds2 = get_keystone_creds()
+    def get_nova_creds():
+        d = {}
+        d['username'] = os.environ['OS_USERNAME']
+        d['api_key'] = os.environ['OS_PASSWORD']
+        d['auth_url'] = os.environ['OS_AUTH_URL']
+        d['project_id'] = os.environ['OS_TENANT_NAME']
+        return d
 
-nova = nvclient.Client(**creds)
-keystone = client.Client(**creds2)
+    creds = get_nova_creds()
+    creds2 = get_keystone_creds()
+
+    nova = nvclient.Client(**creds)
+    keystone = client.Client(**creds2)
