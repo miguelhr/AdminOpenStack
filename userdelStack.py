add43c944b6e2ee8b2e3ce14648b32784606859e
Miguel Hernández Romero <miguel2hermanas@gmail.com>
2014-04-14 16:29:14 +0200

    Completado eliminar grupos de seguridad

--- userdelStack.py
+++ userdelStack.py
@@ -25,31 +25,40 @@ else:
         d['auth_url'] = os.environ['OS_AUTH_URL']
         d['project_id'] = os.environ['OS_TENANT_NAME']
         return d
-
+        
+    def get_credentials():
+        d = {}
+        d['username'] = os.environ['OS_USERNAME']
+        d['password'] = os.environ['OS_PASSWORD']
+        d['auth_url'] = os.environ['OS_AUTH_URL']
+        d['tenant_name'] = os.environ['OS_TENANT_NAME']
+        return d
+        
     creds = get_nova_creds()
     creds2 = get_keystone_creds()
+    credentials = get_credentials()
 
 
     nova = nvclient.Client(**creds)
     keystone = client.Client(**creds2)
-    neutron = neuclient.Client(**creds)
+    neutron = client.Client(**credentials)
 
-#Obtener informacion de usuario
+#Obtener informacion de usuario.
 infousuario = keystone.users.find(name=sys.argv[1])
 listatenant=[];
 
-#Obtener proyectos del usuario elegido
-print "El usuario %s tiene los proyectos" %  infousuario.name
+#Obtener proyectos del usuario elegido.
+print "El usuario %s tiene los proyectos:" %  infousuario.name
 for tenant in keystone.tenants.list():
     for tenant_user in tenant.list_users():
         if infousuario.id in tenant_user.id:
             listatenant.append(tenant.id)
             print tenant.name
 
-#Que proyectos pertenecen a mas de un usuario
+#Que proyectos pertenecen a mas de un usuario.
 for a in listatenant:
     if len(keystone.tenants.list_users(tenant=a)) > 1:
-        print "No se puede borrar el proyecto con id %s porque pertenece a mas de un usuario" % a
+        print "No se puede borrar el proyecto con id %s porque pertenece a mas de un usuario." % a
         print "Los usuarios son:"
         usutenant=keystone.tenants.list_users(tenant=a)
         for i in usutenant:
@@ -57,13 +66,21 @@ for a in listatenant:
     else:
         print "se va a eliminar el proyecto con id %s" % a
 
-#Eliminar todas las instancias asociadas a un proyecto
-        print "El proyecto %s tiene las instancias" %  a
+#Eliminar todas las instancias asociadas a un proyecto.
+        print "El proyecto %s tiene las instancias:" %  a
         totalinstancias=nova.servers.list(search_opts={'all_tenants': True})
-        listainstancias=[];
         for instancias in totalinstancias:
             if a in instancias.tenant_id:
-                listainstancias.append(instancias.tenant_id)
                 print instancias.name
                 nova.servers.delete(instancias.id)
+
+#Eliminar todas los grupos de seguridad asociadas a un proyecto.
+        print "El proyecto %s tiene los grupos de seguridad:" %  a
+        totalgruposeguridad = neutron.list_security_groups()
+        for i in totalgruposeguridad:
+            for b in sg[i]:
+                if a in b['tenant_id']:
+                print b['name']
+                neutron.delete_security_group(b['id'])
+
 
